2277506ca23207af600360f5afb6522f03b39399
Jesdhy <pinchajessy529@gmail.com>
2024-11-15 06:51:27 -0500

    feat: :lipstick: Add new message
    Add new message for the file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// index.js
 const express = require('express');
+
 const app = express();
-const port = process.env.PORT || 3000;  // port
+
+const port = process.env.PORT || 3030;  
 
 app.get('/', (req, res) => {
-  res.send('Hello World JavaScript');
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
